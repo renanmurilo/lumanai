d02d01868e6572dad28c52d1ce4d24bd65f6b9d5
renanmurilo <renanmurilosantosg@gmail.com>
2019-07-31 17:36:52 -0300

    Project

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 p-0">
+        <header>
+          <b-navbar toggleable="lg">
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav>
+                <b-nav-item href="#" to="/">Home</b-nav-item>
+                <b-nav-item href="#" to="/post">Novo Post</b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar>
+        </header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #000000;
+}
+
+.navbar {
+  margin: 0 100px;
+}
+#nav-collapse {
+  text-transform: uppercase;
+  justify-content: flex-start;
+}
+
+li {
+  padding: 15px 18px;
+}
+
+.navbar-brand {
+  font-size: 28px;
+  line-height: 24px;
+  font-weight: bold;
+  /* border: 5px solid #000000; */
+  padding: 4px;
+}
+
+.brand {
+  padding-left: 60px;
+}
+
+/* .navbar .navbar-brand:hover {
+  color: #ffffff;
+  transition: all 0.3s ease;
+} */
+
+.navbar-light .navbar-nav .nav-link {
+  color: #fff;
+  font-weight: bold;
+  letter-spacing: 2px;
+  font-size: 18px;
+}
+.navbar .navbar-nav .nav-link.router-link-exact-active,
+.navbar-light .navbar-nav .nav-link:hover {
+  color: #fff;
+  transition: all 0.3s ease;
+}
+
+li a {
+  color: #fff;
+}
+
+@media screen and (max-width: 992px) {
+  .navbar {
+    margin: 0 auto;
+  }
+  li {
+    padding: 0px;
+  }
+}
+</style>
---
+++ src/views/Post.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Posts</p>
+</template>
+
+<script>
+export default {
+  name: "Posts"
+};
+</script>
+
+<style>
+</style>
